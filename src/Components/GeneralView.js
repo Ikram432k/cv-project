58f1437b024070d71c8468e862936f7c50a5d5b3
ikram mohideen <ikram432k@gmail.com>
2022-05-05 09:10:13 +0530

    0128src/Components/GeneralView.js

--- src/Components/GeneralView.js
+++ src/Components/GeneralView.js
@@ -6,6 +6,9 @@ const GeneralView = (props) =>{
       return(
         <div>
             <h3>{info.Fname}</h3>
+            <h3>{info.Lname}</h3>
+            <p>{info.Pnumber}</p>
+            <p>{info.email}</p>
         </div>
       ); 
   };
