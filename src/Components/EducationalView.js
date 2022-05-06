0b6e8e7a836eefb651fc46ee4aeed39dd257745c
ikram mohideen <ikram432k@gmail.com>
2022-05-06 18:40:51 +0530

    src/Components/EducationalView.js

--- src/Components/EducationalView.js
+++ src/Components/EducationalView.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 const EducationalView = (props) =>{
-const { infos } = props;
+// const { infos } = props;
 return(
     <div>
-        {infos.map((info)=>{
+        {props.infos.map((info,i)=>{
         return(
-        <div >
+        <div key={i}>
             <h3>{info.Sname}</h3>
             <h3>{info.title}</h3>
             <p>{info.Sfrom}-{info.Sto}</p>
+            <div className="editBtn">
+                <button onClick={props.edit} id={i}></button>
+            </div>
         </div>
         )})}
+
     </div>
 )
 }
