062a2c136b6ffe466dacdb2d3d0310568e6674d2
ikram mohideen <ikram432k@gmail.com>
2022-05-08 21:49:41 +0530

    src/Components/EducationalView.js

--- src/Components/EducationalView.js
+++ src/Components/EducationalView.js
@@ -7,10 +7,11 @@ return(
         {props.infos.map((info,i)=>{
         return(
         <div key={i}>
-            <h3>{info.Sname}</h3>
             <h3>{info.title}</h3>
             <p>{info.Sfrom}-{info.Sto}</p>
+            <h3>{info.Sname}</h3>
             <div className="editBtn">
+            <span className="material-icons editIcon">create</span>
                 <button onClick={props.edit} id={i}></button>
             </div>
         </div>
