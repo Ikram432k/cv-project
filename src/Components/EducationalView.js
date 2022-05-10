aa488f5768be221c4e86c1de5977de2d00307ae2
ikram mohideen <ikram432k@gmail.com>
2022-05-10 07:39:55 +0530

    src/Components/EducationalView.js

--- src/Components/EducationalView.js
+++ src/Components/EducationalView.js
@@ -10,9 +10,10 @@ return(
             <h3>{info.title}</h3>
             <p>{info.Sfrom}-{info.Sto}</p>
             <h3>{info.Sname}</h3>
-            <div className="editBtn">
-            <span className="material-icons editIcon">create</span>
-                <button onClick={props.edit} id={i}></button>
+            <div >
+                <button className="editBtn" onClick={props.edit} id={i}>
+                <span className="material-icons editIcon">create</span>
+                </button>
             </div>
         </div>
         )})}
