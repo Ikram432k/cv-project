7e7ea64f6f06bf5672872ae9b919a18304530cbc
ikram mohideen <ikram432k@gmail.com>
2022-05-13 21:32:36 +0530

    src/Components/generalInfo.js

--- src/Components/generalInfo.js
+++ src/Components/generalInfo.js
@@ -1,73 +1,115 @@
-import { Component } from "react";
+// import { Component } from "react";
 // import uniqid from "uniqid";
 import GeneralView from "./GeneralView";
+import { useState } from "react";
 
-class General extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-          className:'',
-          info:{
-            Fname:'',
-            Lname:'',
-            email:'',
-            Pnumber:'',
-          },
-          infos:[],
-          index:'',
-          edited:true,
-        };
-        // this.handleChange = this.handleChange.bind(this)
-    }
+// class General extends Component{
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//           className:'',
+//           info:{
+//             Fname:'',
+//             Lname:'',
+//             email:'',
+//             Pnumber:'',
+//           },
+//           infos:[],
+//           index:'',
+//           edited:true,
+//         };
+//         this.handleChange = this.handleChange.bind(this)
+//     }
 
-    handleChange=(e)=>{
-      this.setState({
-           info:{...this.state.info,
-            [e.target.name]:e.target.value,
-           },
-        });
-    };
-    onSubmitform =(e)=>{
+//     handleChange=(e)=>{
+//       this.setState({
+//            info:{...this.state.info,
+//             [e.target.name]:e.target.value,
+//            },
+//         });
+//     };
+//     onSubmitform =(e)=>{
+//       e.preventDefault();
+//       let temp = this.state.info
+//       this.setState({
+//         info:{...temp}
+//       })
+//         console.log(this.state.info)
+//     this.closeForm()   
+//     };
+//     openForm = ()=>{
+//       this.setState({className:''});
+//     }
+//     closeForm = (e)=>{
+//       this.setState({className:'unactive'})
+//     }
+//     editForm=()=>{
+//       this.openForm()
+//     }
+//     render(){
+//       const{ info } = this.state;
+
+//         return(
+//             <div className='general'>
+//             <h2 className="subTitle">General Information</h2>
+//             <GeneralView info={info} edit={this.editForm}/>
+//             <form className={this.state.className}>
+//               <label htmlFor='g-Fname'>First Name:</label>
+//               <input onChange={this.handleChange} value={info.Fname} type="text" className='input g-Fname' name="Fname" required/>
+//               <label htmlFor='g-Lname'>Second Name:</label>
+//               <input onChange={this.handleChange} value={info.Lname} type="text" className='input g-Lname' name="Lname" required/>
+//               <label htmlFor='g-email'>Email:</label>
+//               <input onChange={this.handleChange} value={info.email} type="text" className='input g-email' name="email" required/>
+//               <label htmlFor='g-Pnumber'>Phone Number</label>
+//               <input onChange={this.handleChange} value={info.Pnumber} type="text" className='input g-Pnumber' name="Pnumber" required/>
+//               <button onClick={this.onSubmitform} className='formBtn'>Save</button>
+//             </form>
+//           </div>
+//         )
+//     }
+// }
+// export default General;
+
+function General(){
+  const [info,setInfo]=useState(
+    {Fname:'',
+    Lname:'',
+    email:'',
+    Pnumber:'',}
+  );
+  let [className,setClassName]=useState('');    
+
+    const handleChange=(e)=>{
+        setInfo({...info,[e.target.name]:e.target.value}) 
+      };
+    const onSubmitform =(e)=>{
       e.preventDefault();
-      let temp = this.state.info
-      this.setState({
-        info:{...temp}
-      })
-        console.log(this.state.info)
-    this.closeForm()   
+      setClassName((className='unactive'));
     };
-    openForm = ()=>{
-      this.setState({className:''});
-    }
-    closeForm = (e)=>{
-      this.setState({className:'unactive'})
-    }
-    editForm=()=>{
-      this.openForm()
-    }
-    render(){
-      const{ info } = this.state;
+    const  editForm=()=>{
+      setClassName({className:''})};
+      
+    const {Fname,Lname,Pnumber,email}=info
 
-        return(
+    return(
             <div className='general'>
             <h2 className="subTitle">General Information</h2>
-            <GeneralView info={info} edit={this.editForm}/>
-            <form className={this.state.className}>
+            <GeneralView info={info} edit={editForm}/>
+            <form onSubmit={onSubmitform} className={className} >
               <label htmlFor='g-Fname'>First Name:</label>
-              <input onChange={this.handleChange} value={info.Fname} type="text" className='input g-Fname' name="Fname" required/>
+              <input onChange={handleChange} value={Fname} type="text" className='input g-Fname' name="Fname" required/>
               <label htmlFor='g-Lname'>Second Name:</label>
-              <input onChange={this.handleChange} value={info.Lname} type="text" className='input g-Lname' name="Lname" required/>
+              <input onChange={handleChange} value={Lname} type="text" className='input g-Lname' name="Lname" required/>
               <label htmlFor='g-email'>Email:</label>
-              <input onChange={this.handleChange} value={info.email} type="text" className='input g-email' name="email" required/>
+              <input onChange={handleChange} value={email} type="text" className='input g-email' name="email" required/>
               <label htmlFor='g-Pnumber'>Phone Number</label>
-              <input onChange={this.handleChange} value={info.Pnumber} type="text" className='input g-Pnumber' name="Pnumber" required/>
-              <button onClick={this.onSubmitform} className='formBtn'>Save</button>
-              {/* <button onClick={this.openForm}>Delete</button> */}
+              <input onChange={handleChange} value={Pnumber} type="text" className='input g-Pnumber' name="Pnumber" required/>
+              <div className="formEditBtn">
+              <button type="submit" className='btn formBtn'>Save</button>
+              </div>
             </form>
-            {/* <button onClick={this.openForm}>Add</button> */}
-
           </div>
         )
-    }
 }
+
 export default General;
