88746b77b2572c18f23a5d314bc341195078a277
ikram mohideen <ikram432k@gmail.com>
2022-05-13 21:31:38 +0530

    src/Components/GeneralView.js

--- src/Components/GeneralView.js
+++ src/Components/GeneralView.js
@@ -1,27 +1,27 @@
 import React from "react";
 
 const GeneralView = (props) =>{
-      const { info } = props;
+const { info } = props;
   
-      return(
-        <div>
-        <div>
-            <h1>{info.Fname} {info.Lname}</h1>
+return(
+        <div className="container">
+          <div className="contactList">
+            <h1 id="items">{info.Fname} {info.Lname}</h1>
             <div className="contact">
-            <i className="material-icons">call</i>
-            <p>{info.Pnumber}</p>
+            {/* <i className="material-icons">call</i> */}
+            <p id="items">{info.Pnumber}</p>
             </div>
             <div className="contact">
-            <i className="material-icons">mail_outline</i>
-            <p>{info.email}</p>
-        </div>
-        </div>
+            {/* <i className="material-icons">mail_outline</i> */}
+            <p id="items">{info.email}</p>
+           </div>
+           </div>
               <div className="edit">
                     <button className="editBtn" onClick={props.edit}>
                     <span className="material-icons editIcon">create</span>
                     </button>
-                </div>
+              </div>
         </div>
-      ); 
-  };
+      )
+  }
 export default GeneralView;
