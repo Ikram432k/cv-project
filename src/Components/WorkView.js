b37eeaf2e453a3207b09c744daeddfedd59d83fd
ikram mohideen <ikram432k@gmail.com>
2022-05-13 21:31:57 +0530

    src/Components/WorkView.js

--- src/Components/WorkView.js
+++ src/Components/WorkView.js
@@ -3,14 +3,16 @@ import React from "react";
 const WorkView = (props) =>{
 // const { infos } = props;
 return(
-    <div>
+    <div >
         {props.infos.map((info,i)=>{
         return(
-        <div key={i}>
-            <h3>{info.Wname}</h3>
-            <h3>{info.Ptitle}</h3>
-            <p>{info.Wfrom} - {info.Wto}</p>
-            <p>{info.tasks}</p>
+            <div key={i} className="container">
+            <div className="containerList">
+            <h3 id="items">{info.Wname}</h3>
+            <h3 id="items">{info.Ptitle}</h3>
+            <p id="items">{info.Wfrom} - {info.Wto}</p>
+            <p id="items">{info.tasks}</p>
+            </div>
             <div className="edit">
                 <button className="editBtn" onClick={props.edit} id={i}>
                 <span className="material-icons editIcon">create</span>
